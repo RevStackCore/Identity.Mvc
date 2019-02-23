c81c8faff429525d1d382652d928decfd74c3a14
Scott <sfrancis@misinteractive.com>
2019-02-22 21:51:02 -0500

    Updated version

--- RevStackCore.Identity.Mvc/Model/BaseApplicationUser.cs
+++ RevStackCore.Identity.Mvc/Model/BaseApplicationUser.cs
@@ -4,13 +4,13 @@ namespace RevStackCore.Identity.Mvc
 {
     public class BaseApplicationUser : IdentityUser
     {
-		public string FirstName { get; set; }
-		public string LastName { get; set; }
-		public string City { get; set; }
-		public string State { get; set; }
-		public string Street { get; set; }
-		public string ZipCode { get; set; }
-        public string Avatar { get; set; }
-        public string MobilePhoneNumber { get; set; }
+		public virtual string FirstName { get; set; }
+		public virtual string LastName { get; set; }
+		public virtual string City { get; set; }
+		public virtual string State { get; set; }
+		public virtual string Street { get; set; }
+		public virtual string ZipCode { get; set; }
+        public virtual string Avatar { get; set; }
+        public virtual string MobilePhoneNumber { get; set; }
     }
 }
